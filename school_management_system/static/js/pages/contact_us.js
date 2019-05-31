ba83178964931a7ed069227899ad8da3659e9608
Nagarajaiah <nhm@infoblox.com>
2019-05-31 11:53:15 +0530

    Dispaly the error message

--- school_management_system/static/js/pages/contact_us.js
+++ school_management_system/static/js/pages/contact_us.js
@@ -9,10 +9,7 @@ app.config(['$httpProvider', '$interpolateProvider',
 //Register form controller
 app.controller('contCtrl', function($scope,$http,$window) {
 		$scope.submitform = function() {
-			console.log("ss"+contact_usForm);
-
-		
-                if ($scope.contact_usForm.$valid) {
+   			if ($scope.contact_usForm.$valid) {
                    var contact_us = JSON.stringify($scope.contact_us);
                       $http({
 						method : "POST",
@@ -22,16 +19,11 @@ app.controller('contCtrl', function($scope,$http,$window) {
 				console.log("query worked") 
 				alert("Query is delivered .we will contact you soon") 
 			},function(response) {		
-				$scope.error = response.data.Email_id;
-				$scope.esaefsefrror = response.data.mobile_number;
-				//$scope.error= response.data.mobile_number;
+				$scope.email_error = response.data.Email_id;
+				$scope.mobile_error = response.data.mobile_number;
+				
 			});
-			// },function(response) {		
-			// 	$scope.error = response.data.mobile_number;
-			// });               
-
-                        
-                } else{
+					} else{
                         console.log("Invalid")                
                         return;
                 }
