2bfc512b248d0636e2630d4e047172bce3cd2e66
Nagarajaiah <nhm@infoblox.com>
2019-06-28 11:16:55 +0530

    added the comment line

--- school_management_system/urls.py
+++ school_management_system/urls.py
@@ -5,12 +5,16 @@ urlpatterns = [
 
     url(r'^home/$', views.home, name='home'),
     url(r'^about_us/$',views.about_us,name='about_us'),
-    url(r'^login/$', views.user_login, name='user_login'),
+    url(r'^introduction/$', views.introduction, name='introduction'),
+    # faculty Registration
     url(r'^faculty/$', views.faculty_List, name='faculty'),
     url(r'^faculty_registration/$', views.fac_registration, name='fac_registration'),
+    # Student registration and login
   	url(r'^student_Registration/$', views.stu_Registration, name='student_Registration'),
     url(r'^dashboard/$', views.student_dashboard, name='student_dashboard'),
-    url(r'^introduction/$', views.introduction, name='introduction'),
+    url(r'^login/$', views.user_login, name='user_login'),
+    
+    # contact_us
     url(r'^contact_us/$', views.contact_us, name='contact_us'),
     url(r'^terms/$', views.terms_conditions, name='terms'),
     #admin re
